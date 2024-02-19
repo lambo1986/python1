583fba3ded6a837cde49010d23a87579bc4d5d7d
Nathan Lambertson <lambertones@gmail.com>
2024-02-19 12:00:18 -0600

    initial commit

---
+++ lib/experiemt.py
@@ -0,0 +1,19 @@
+def add_numbers(a, b):
+  return a + b
+
+print(add_numbers(1, 2))
+
+def multiply_numbers(a, b):
+  return a * b
+
+print(multiply_numbers(1, 2))
+
+def subtract_numbers(a, b):
+  return a - b
+
+print(subtract_numbers(1, 2))
+
+def divide_numbers(a, b):
+  return a / b 
+
+print(divide_numbers(1, 2))
