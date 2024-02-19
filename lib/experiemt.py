238d685199c41807018f6da9a8ee1657e8c440ef
Nathan Lambertson <lambertones@gmail.com>
2024-02-19 12:07:41 -0600

    Add some more basic equations

--- lib/experiemt.py
+++ lib/experiemt.py
@@ -17,3 +17,24 @@ def divide_numbers(a, b):
   return a / b 
 
 print(divide_numbers(1, 2))
+
+def square_numbers(a):
+  return a * a
+
+print(square_numbers(2455454))
+
+def cube_numbers(a):
+  return a * a * a
+
+print(cube_numbers(2455454))
+
+def power_numbers(a, b):
+  return a ** b
+
+print(power_numbers(2455454, 7))
+
+def remainder_numbers(a, b):
+  return a % b
+
+print(remainder_numbers(8, 7))
+
